2301a63b5b3e6fd893b59a39a2dfa0a3ffc97400
JsIqbal <javascriptiqbal@gmail.com>
2024-06-20 21:56:34 +0600

    Use Basic Redis Client get/set

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,19 @@
-export const getCachedPage = (route: string) => {};
+import { client } from "$services/redis";
 
-export const setCachedPage = (route: string, page: string) => {};
+const cacheRoutes = ["/privacy", "/about", "/auth/signin", "/auth/signup"];
+
+export const getCachedPage = (route: string) => {
+    if (cacheRoutes.includes(route)) {
+        return client.get("pagecache#" + route);
+    }
+
+    return null;
+};
+
+export const setCachedPage = (route: string, page: string) => {
+    if (cacheRoutes.includes(route)) {
+        client.set("pagecache#" + route, page, {
+            EX: 2,
+        });
+    }
+};

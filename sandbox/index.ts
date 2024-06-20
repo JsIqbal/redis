8969e30dc824b2f688484e1eaf8f2e67d94085e0
JsIqbal <javascriptiqbal@gmail.com>
2024-06-20 23:23:28 +0600

    Complete Redis has Gotchas

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,26 @@
-import 'dotenv/config';
-import { client } from '../src/services/redis';
+import "dotenv/config";
+import { client } from "../src/services/redis";
 
-const run = async () => {};
+const run = async () => {
+    const key = "company";
+
+    await client.hSet("company", {
+        name: "iq",
+        industry: "tech",
+        age: "12.3",
+        estd: "1965",
+        a: "10.5",
+    });
+
+    const exists = await client.hGetAll(key);
+
+    if (Object.keys(exists).length === 0) {
+        console.log("404: NOT FOUND!");
+
+        return;
+    }
+
+    console.log(exists);
+};
+
 run();

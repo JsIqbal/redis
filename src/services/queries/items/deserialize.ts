cd9ef30e323bd471b11513823dfd2158935aa97b
JsIqbal <javascriptiqbal@gmail.com>
2024-06-23 23:33:03 +0600

    Get an Item

--- src/services/queries/items/deserialize.ts
+++ src/services/queries/items/deserialize.ts
@@ -1,4 +1,22 @@
-import type { Item } from '$services/types';
-import { DateTime } from 'luxon';
+import type { Item } from "$services/types";
+import { DateTime } from "luxon";
 
-export const deserialize = (id: string, item: { [key: string]: string }): Item => {};
+export const deserialize = (
+    id: string,
+    item: { [key: string]: string }
+): Item => {
+    return {
+        id: id,
+        highestBidUserId: item.highestBidUserId,
+        name: item.name,
+        ownerId: item.ownerId,
+        imageUrl: item.imageUrl,
+        description: item.description,
+        createdAt: DateTime.fromMillis(parseInt(item.createdAt)),
+        endingAt: DateTime.fromMillis(parseInt(item.endingAt)),
+        views: parseInt(item.views),
+        likes: parseInt(item.likes),
+        price: parseFloat(item.price),
+        bids: parseInt(item.bids),
+    };
+};

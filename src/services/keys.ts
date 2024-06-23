ff38ba6242465f100dad45b6f1812d0ca0f04f22
JsIqbal <javascriptiqbal@gmail.com>
2024-06-23 23:15:28 +0600

    Create A Item

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -4,3 +4,5 @@ export const usersKey = (userId: string): string => `users#${userId}`;
 
 export const sessionsKey = (sessionId: string): string =>
     `sessions#${sessionId}`;
+
+export const itemsKey = (itemsId: string) => `items#${itemsId}`;

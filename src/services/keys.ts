be3917ff8158066ae6646fc97c4ec6474b78ca6f
JsIqbal <javascriptiqbal@gmail.com>
2024-06-23 22:52:14 +0600

    Complete Session Create and Get

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,3 +1,6 @@
 export const pageCacheKey = (route: string): string => `pagecache#${route}`;
 
 export const usersKey = (userId: string): string => `users#${userId}`;
+
+export const sessionsKey = (sessionId: string): string =>
+    `sessions#${sessionId}`;

83240b5bd818d0bd7fee8e314bfb2727cb72f759
JsIqbal <javascriptiqbal@gmail.com>
2024-06-28 13:38:23 +0600

    Implement Pipeline Commands

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -4,7 +4,7 @@ import { client } from "../src/services/redis";
 const run = async () => {
     const key = "company";
 
-    await client.hSet("company", {
+    await client.hSet("company1", {
         name: "iq",
         industry: "tech",
         age: "12.3",
@@ -12,7 +12,30 @@ const run = async () => {
         a: "10.5",
     });
 
-    const exists = await client.hGetAll(key);
+    await client.hSet("company2", {
+        name: "iq",
+        industry: "tech",
+        age: "12.3",
+        estd: "1965",
+        a: "10.5",
+    });
+
+    await client.hSet("company3", {
+        name: "iq",
+        industry: "tech",
+        age: "12.3",
+        estd: "1965",
+        a: "10.5",
+    });
+
+    const commands = [1, 2, 3].map((id) => {
+        return client.hGetAll(`company${id}`);
+    });
+
+    console.log("----commands----", commands);
+
+    const exists = await Promise.all(commands);
+    console.log("----commands----", commands);
 
     if (Object.keys(exists).length === 0) {
         console.log("404: NOT FOUND!");

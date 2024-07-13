2ab61df9c8ae81699aa4920ee6f47c521341f584
JsIqbal <javascriptiqbal@gmail.com>
2024-07-13 18:57:07 +0600

    Work on User Like Feature

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,10 +1,6 @@
 export const pageCacheKey = (route: string): string => `pagecache#${route}`;
-
 export const usersKey = (userId: string): string => `users#${userId}`;
-
-export const sessionsKey = (sessionId: string): string =>
-    `sessions#${sessionId}`;
-
+export const sessionsKey = (sessionId: string): string => `sessions#${sessionId}`;
 export const itemsKey = (itemsId: string): string => `items#${itemsId}`;
-
 export const usernamesUniqueKey = (): string => 'usernames:unique';
+export const usersLikesKey = (userId: string) => `users:like#${userId}`;
--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,19 @@
+import { usersLikesKey } from "$services/keys";
+import { client } from "$services/redis";
+
 export const userLikesItem = async (itemId: string, userId: string) => {};
 
 export const likedItems = async (userId: string) => {};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+    await client.sAdd(usersLikesKey(userId), itemId);
+};
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+export const unlikeItem = async (itemId: string, userId: string) => {
+    await client.sRem(usersLikesKey(userId), itemId);
+};
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const commonLikedItems = async (
+    userOneId: string,
+    userTwoId: string
+) => {};

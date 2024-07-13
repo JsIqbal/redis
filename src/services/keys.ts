ad066e78e66c2f9fa829142493fb40cd2439da41
JsIqbal <javascriptiqbal@gmail.com>
2024-07-13 19:34:14 +0600

    Show user Like and dislike

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,6 +1,7 @@
 export const pageCacheKey = (route: string): string => `pagecache#${route}`;
 export const usersKey = (userId: string): string => `users#${userId}`;
-export const sessionsKey = (sessionId: string): string => `sessions#${sessionId}`;
+export const sessionsKey = (sessionId: string): string =>
+    `sessions#${sessionId}`;
 export const itemsKey = (itemsId: string): string => `items#${itemsId}`;
-export const usernamesUniqueKey = (): string => 'usernames:unique';
-export const usersLikesKey = (userId: string) => `users:like#${userId}`;
+export const usernamesUniqueKey = (): string => "usernames:unique";
+export const userLikesKey = (userId: string) => `users:like#${userId}`;

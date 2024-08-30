43177d1e6a1ab697e52210fe1c9c5291586365a0
JsIqbal <javascriptiqbal@gmail.com>
2024-08-30 13:59:49 +0600

    Complete section 11

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,7 +1,6 @@
 export const pageCacheKey = (route: string): string => `pagecache#${route}`;
 export const usersKey = (userId: string): string => `users#${userId}`;
-export const sessionsKey = (sessionId: string): string =>
-    `sessions#${sessionId}`;
+export const sessionsKey = (sessionId: string): string => `sessions#${sessionId}`;
 export const itemsKey = (itemsId: string): string => `items#${itemsId}`;
 export const usernamesUniqueKey = (): string => "usernames:unique";
 export const userLikesKey = (userId: string) => `users:like#${userId}`;

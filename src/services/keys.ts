96f590776420ae6c1fda23f29faeedb7299a6bcf
JsIqbal <javascriptiqbal@gmail.com>
2024-10-07 02:17:35 +0600

    Add Username Key

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,6 +1,8 @@
 export const pageCacheKey = (route: string): string => `pagecache#${route}`;
 export const usersKey = (userId: string): string => `users#${userId}`;
-export const sessionsKey = (sessionId: string): string => `sessions#${sessionId}`;
+export const sessionsKey = (sessionId: string): string =>
+    `sessions#${sessionId}`;
 export const itemsKey = (itemsId: string): string => `items#${itemsId}`;
 export const usernamesUniqueKey = (): string => "usernames:unique";
 export const userLikesKey = (userId: string) => `users:like#${userId}`;
+export const usernamesKey = () => "username";

5bd10fbf8ec4796da386c3b36e9984e965e95808
JsIqbal <javascriptiqbal@gmail.com>
2024-10-11 16:22:29 +0600

    Implementing Items Views Cache
    In the file key.ts I have created a itemsByViews Key. Then in items.ts file I implemented sorted set in createItem method. I pipelined the redis client requests.

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -2,7 +2,9 @@ export const pageCacheKey = (route: string): string => `pagecache#${route}`;
 export const usersKey = (userId: string): string => `users#${userId}`;
 export const sessionsKey = (sessionId: string): string =>
     `sessions#${sessionId}`;
-export const itemsKey = (itemsId: string): string => `items#${itemsId}`;
 export const usernamesUniqueKey = (): string => "usernames:unique";
 export const userLikesKey = (userId: string) => `users:like#${userId}`;
 export const usernamesKey = () => "username";
+
+export const itemsKey = (itemsId: string): string => `items#${itemsId}`;
+export const itemsByViewsKey = (): string => "items:view";
